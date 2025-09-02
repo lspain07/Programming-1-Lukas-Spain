83daa1023c16f76d9eb5f440ce7c1ed1c85f4e87
lspain07 <lukasrspain@gmail.com>
2025-09-01 20:31:26 -0500

    clank

--- SpainL_Chapter2Practice.cpp
+++ SpainL_Chapter2Practice.cpp
@@ -16,6 +16,10 @@ int main()
 	char mInitial;
 	int age;
 	double gpa;
+	string major;
+	string class;
+	int gradYear;
+	char grade;
 
 	//program title and created by
 	cout << "Chapter 2 Practice Program \n";
@@ -23,7 +27,8 @@ int main()
 
 	//program description/instructions
 	cout << "This program will ask the user for the first and last name, middle initial, age, \n";
-	cout << "and current gpa. We will then print that information back to the console.\n\n";
+	cout << "current gpa, major, class, expected graduation year, and grade they hope to receive. \n";
+	cout << "We will then print that information back to the console.\n\n";
 
 	//Collect user input
 	cout << "Enter your first name: ";
@@ -36,16 +41,24 @@ int main()
 	cin >> age;
 	cout << "Enter your current GPA (ex: 3.75): ";
 	cin >> gpa;
+	cout << "Enter your major: ";
+	cin >> major;
+	cout << "Enter your class (freshman, sophomore, junior, senior): ";
+	cin >> class;
+	cout << "Enter your expected graduation year: ";
+	cin >> gradYear;
+	cout << "Enter what letter grade you hope to receive in this class (A, B, C, D, F): ";
+	cin >> grade;
 
 	//output to console
 	cout << endl << endl;
 	cout << fName << " " << mInitial << ". " << lName << endl;
 	cout << "Your age is: " << age << endl;
 	cout << "Your current GPA is: " << gpa << endl;
-
-
-
-
+	cout << "Your major is: " << major << endl;
+	cout << "You are a: " << class << endl;
+	cout << "You expect to graduate in: " << gradYear << endl;
+	cout << "You hope to receive a(n): " << grade << " in this class." << endl;
 
     return 0;
 }
