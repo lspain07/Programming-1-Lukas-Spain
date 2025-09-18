9be735cb0293dfead73a0e81f590148ab417a9c5
lspain07 <lukasrspain@gmail.com>
2025-09-18 15:02:29 -0500

    Uploading all work

---
+++ Favorites.cpp
@@ -0,0 +1,28 @@
+// Favorites.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Lukas Spain
+// 2025-08-26
+// Favorites Version 1
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Lukas Spain's Favorite Things\n\n";
+
+    cout << "Food: chinese dumplings\n";
+    cout << "Drink: arizona tea\n";
+    cout << "Movie: Interstellar\n";
+    cout << "Candy: chewy sprees\n";
+    cout << "Color: green\n";
+    cout << "Animal: dog\n";
+    cout << "Class: Engineering\n";
+    cout << "Teacher: Mr. Garin Bean\n";
+    cout << "Day of the week: Friday\n";
+    cout << "Season: Fall\n";
+	cout << "Video Game: Elder Scrolls V: Skyrim\n";
+    cout << "Activity: Band\n";
+
+    return 0;
+}
